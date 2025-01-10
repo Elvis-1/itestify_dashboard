93db586425cc33a2cd44699581aef0d33089d413
edwin chris <officialedwinchris@gmail.com>
2025-01-10 18:13:45 +0100

    bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 
 import React from "react";
-import { Button } from "antd";
-import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreatePassword from "./pages/CreatePassword";
@@ -10,21 +8,11 @@ import Overview from "./component/Overview";
 import { DarkModeContextProvider } from "./context/DarkModeContext";
 import Users from "./pages/Users";
 import Donations from "./pages/Donations";
-import React from 'react';
 import { Button } from 'antd';
-import './App.css'
-import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CreatePassword from './pages/CreatePassword';
-import Dashboard from './pages/Dashboard';
-import Overview from './component/Overview';
-import { DarkModeContextProvider } from './context/DarkModeContext';
 import AllTestimonies from './component/AllTestimonies';
 import UploadTestimonies from './component/UploadTestimonies';
 import VideoPlayer from './component/VideoPlayer';
 
-
- function App() {
 
 function App() {
   return (
@@ -36,22 +24,13 @@ function App() {
             <Route path="/create-password" element={<CreatePassword />} />
             <Route path="/dashboard" element={<Dashboard />}>
               <Route path="" element={<Overview />} />
+              <Route path='all-testimonies' element={<AllTestimonies/>}/>
+              <Route path='upload-testimonies' element={<UploadTestimonies/>}/>
               <Route path="/dashboard/users" element={<Users />} />
               <Route path="/dashboard/donations" element={<Donations />} />
             </Route>
           </Routes>
         </BrowserRouter>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/create-password' element={<CreatePassword/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}>
-              <Route path='' element={<Overview/>}/>
-              <Route path='all-testimonies' element={<AllTestimonies/>}/>
-              <Route path='upload-testimonies' element={<UploadTestimonies/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
       </DarkModeContextProvider>
     </div>
   );
