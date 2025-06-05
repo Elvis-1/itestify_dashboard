bb0010cd6e27838c97aa1bd7d00edfb69220b3c2
FAITH <faithokereke26@gmail.com>
2025-06-05 18:39:17 +0100

    fixed array error

--- src/component/userTypes/RegUsers.jsx
+++ src/component/userTypes/RegUsers.jsx
@@ -200,7 +200,7 @@ const RegUsers = () => {
             </tr>
           </thead>
           {regUsersIndex.length > 0 ? (
-            users.map((data, index) => (
+            (users||[]).map((data, index) => (
               <tbody className="relative" key={data.id}>
                 <tr
                   className={` ${
